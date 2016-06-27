2c33cce1f50169e0c70d434206ad41bbab4e3294
Harry R <harirao001@gmail.com>
2016-06-27 12:13:28 -0400

    fixed sound alert issue when order comes in

--- application/views/v_orderlist.php
+++ application/views/v_orderlist.php
@@ -68,9 +68,12 @@
             .invoice .invoicename{
                 top:90px;
             }
+            .audiojs{width: 1px;height: 1px}
 
 
         </style>
+        <script src="<?php echo base_url('assets/js/audiojs/audio.min.js'); ?>"></script>
+
     </head>
     <body>
         <?php $this->load->view('v_header'); ?>
@@ -174,12 +177,51 @@
 
 
         <?php $this->load->view('v_script'); ?>
-        
-        <audio id="audiotag1">
-            <source src="<?php echo base_url('assets/audio/audio1.ogg'); ?>" type="audio/ogg">
-            <source src="<?php echo base_url('assets/audio/audio1.mp3'); ?>"  type="audio/mpeg">
-            Your browser does not support the audio element.
-        </audio>
+        <audio></audio>
+        <script>
+            $(function () {
+                // Setup the player to autoplay the next track
+                var a = audiojs.createAll({
+                    trackEnded: function () {
+                        audio.pause();
+                    }
+                });
+
+                // Load in the first track
+                var audio = a[0];
+                first = $('ol a').attr('data-src');
+                $('ol li').first().addClass('playing');
+                audio.load(first);
+
+                // Load in a track on click
+                $('#audio1').click(function (e) {
+                    e.preventDefault();
+                    $(this).addClass('playing').siblings().removeClass('playing');
+                    audio.load($('a', this).attr('data-src'));
+                    audio.play();
+                });
+                // Keyboard shortcuts
+                $(document).keydown(function (e) {
+                    var unicode = e.charCode ? e.charCode : e.keyCode;
+                    // right arrow
+                    if (unicode == 39) {
+                        var next = $('li.playing').next();
+                        if (!next.length)
+                            next = $('ol li').first();
+                        next.click();
+                        // back arrow
+                    } else if (unicode == 37) {
+                        var prev = $('li.playing').prev();
+                        if (!prev.length)
+                            prev = $('ol li').last();
+                        prev.click();
+                        // spacebar
+                    } else if (unicode == 32) {
+                        audio.playPause();
+                    }
+                })
+            });
+        </script>
 
         <script>
             window.history.forward(-1);
@@ -237,7 +279,7 @@
                     $("#latest_order_id").val(data['latest_order_id']);
                     $("#count_new_order").html(data['count_new_order']);
                     $("#alertbottom").show();
-                     document.getElementById("audiotag1").play();
+                    $('#audio1').click();
                 }
 
 
@@ -254,7 +296,8 @@
 
                                 $("#count_pending_order").html(data);
                                 $("#alertPendingOrder").show();
-                                document.getElementById("audiotag1").play();
+                                $('#audio1').click();
+
 
                             }
                         });
@@ -275,7 +318,7 @@
                 }, 1000);
             }
         </script>
-
+        <ol style="display: none;"><li id="audio1"><a href="#" data-src="<?php echo base_url('assets/audio/audio1.mp3'); ?>">s</a></li></ol>
 
     </body>
 </html>
