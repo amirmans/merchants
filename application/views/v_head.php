89d5f340170ccfda0f66f5fa35b36a80f493fd33
Harry <harry@Harrys-Mac-mini.local>
2017-02-14 04:04:12 -0500

    - Hide option and product tab when screen width less then 450px

--- application/views/v_head.php
+++ application/views/v_head.php
@@ -4,3 +4,14 @@
 <link href="<?php echo base_url('assets/css/plugin/sweet-alert/sweet-alert.css'); ?>" rel="stylesheet">
 <link href="<?php echo base_url('assets/css/plugin/date-range-picker/daterangepicker.css'); ?>" rel="stylesheet">
 <link href="<?php echo base_url('assets/css/plugin/bootstrap-timepicker/bootstrap-datetimepicker.css'); ?>" rel="stylesheet">
+<style>
+
+@media only screen and (max-width: 450px) {
+   #option_tab {
+       display: none;
+    }
+   #product_tab {
+       display: none;
+    }
+}
+</style>
